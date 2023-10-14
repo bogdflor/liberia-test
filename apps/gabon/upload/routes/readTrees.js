2d5b0508a871f93736517850441384c173d47b47
bogdflor <112245361+bogdflor@users.noreply.github.com>
2023-10-14 10:42:28 +0300

    Update readTrees.js

--- apps/gabon/upload/routes/readTrees.js
+++ apps/gabon/upload/routes/readTrees.js
@@ -6,6 +6,17 @@ export const router = express.Router();
 
 router.get(
     "/api/annual_allowable_cut_inventory/vectors",
+     [
+        check(req.query.zoom)
+                                .exists()
+                                .notEmpty()
+                                .withMessage("zoom must be provided") ,
+         check(req.query.bbox)
+                        .exists()
+                        .notEmpty()
+                        .withMessage("bbox must be provided") ,
+    ],
+    validateRequest,
     async (req, res, next) => {
         try{ 
             const tree = new Tree();
